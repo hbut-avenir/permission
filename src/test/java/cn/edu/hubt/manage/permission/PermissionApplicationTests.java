3c6422b6e9bb0fa3d405e30e8de58f06a83847bd
hbut-avenir <1213598704@qq.com>
2022-08-24 11:04:25 +0800

    初始化。

---
+++ src/test/java/cn/edu/hubt/manage/permission/PermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hubt.manage.permission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
